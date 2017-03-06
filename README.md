ba1c1310145336a5d703f771102cc55dad295a7a
Miguel Cordero <miguelcord06@gmail.com>
2017-03-06 00:42:41 -0500

    Add about me and footer

---
+++ README.md
@@ -0,0 +1,4 @@
+# miguel-port
+Miguel's Final Portfolio
+
+This is the repo that will hold my personal portfolio outside WordPress.
---
+++ index.php
@@ -0,0 +1,11 @@
+ <div class="row">
+                    <div class="col-md-2 left-col">
+                    
+                    </div>
+                    <div class="col-md-8 mid-col">
+                        
+                    </div>
+                    <div class="col-md-2 rigt-col">
+                    
+                    </div>
+                </div>
